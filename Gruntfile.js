60e6a63e3fb1119b08c138d5640349f92fcf1779
Jeremy Green <jgreen@taoti.com>
2014-02-03 09:29:48 -0500

    styling

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,37 +2,35 @@
 
 module.exports = function (grunt) {
 
-    // Project configuration.
-    grunt.initConfig({
-      jshint: {
-        all: [
-          'Gruntfile.js',
-          'tasks/*.js'
-        ],
-        options: {
-          jshintrc: '.jshintrc',
-        },
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js'
+      ],
+      options: {
+        jshintrc: '.jshintrc',
       },
-      lynx: {
-        options: {
-          auth: {
-            username: '',
-            password: ''
-          },
-          url: 'localhost',
-          dest: 'output/site.txt'
-        }
+    },
+    lynx: {
+      options: {
+        auth: {
+          username: '',
+          password: ''
+        },
+        url: 'localhost',
+        dest: 'output/site.txt'
       }
-    });
+    }
+  });
 
-    // Actually load this plugin's task(s).
+  // Actually load this plugin's task(s).
   grunt.loadTasks('tasks');
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
 
-  // By default, lint and run all tests.
+  // Register default task
   grunt.registerTask('default', ['jshint']);
-
 };
